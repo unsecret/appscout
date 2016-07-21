c98195191d8b76a93db8166b4fdee5a725db732c
unsecret <github@gots.biz>
2016-07-21 21:24:30 +0200

    Added on click event on one item

--- app/src/main/java/secretskylab/appscout/MainActivity.java
+++ app/src/main/java/secretskylab/appscout/MainActivity.java
@@ -1,7 +1,11 @@
 package secretskylab.appscout;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // if button is pressed !
+        final Button button = (Button) findViewById(R.id.rockScout);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.carconnectivity.mlmediaplayer")));
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.carconnectivity.mlmediaplayer")));
+                }
+            }
+        });
     }
 }
